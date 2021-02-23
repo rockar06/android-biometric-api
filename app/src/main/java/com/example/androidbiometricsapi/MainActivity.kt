f28288ce7c3063e0f468997d7bc1f9779f98c4ec
Armando Ruiz <aranruiz13@gmail.com>
2021-02-22 18:40:13 -0600

    Add logic in MainActivity.kt to use biometrics helper

--- app/src/main/java/com/example/androidbiometricsapi/MainActivity.kt
+++ app/src/main/java/com/example/androidbiometricsapi/MainActivity.kt
@@ -1,38 +1,159 @@
 package com.example.androidbiometricsapi
 
+import android.content.Intent
+import android.hardware.biometrics.BiometricManager
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import android.provider.Settings
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import androidx.biometric.BiometricPrompt
+import com.example.androidbiometricsapi.biometric.*
+import com.example.androidbiometricsapi.crypto.CryptographyManagerImpl
+import com.example.androidbiometricsapi.crypto.EncryptedData
+import com.example.androidbiometricsapi.databinding.ActivityMainBinding
+
+private const val REQUEST_CODE = 1
+private const val EMPTY_STRING = ""
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+    private lateinit var binding: ActivityMainBinding
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+    private var biometricType: BiometricType = None
+    private val cryptographyManager = CryptographyManagerImpl()
+    private lateinit var cipherText: ByteArray
+    private lateinit var initializationVector: ByteArray
+    private val biometricHelper: BiometricHelper by lazy {
+        BiometricHelperImpl(this, getBiometricCallback())
+    }
+
+    private fun getBiometricCallback(): BiometricPrompt.AuthenticationCallback {
+        return object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationError(
+                errorCode: Int,
+                errString: CharSequence
+            ) {
+                super.onAuthenticationError(errorCode, errString)
+                Toast.makeText(
+                    this@MainActivity,
+                    "Authentication error: $errString", Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+
+            override fun onAuthenticationSucceeded(
+                result: BiometricPrompt.AuthenticationResult
+            ) {
+                super.onAuthenticationSucceeded(result)
+                Toast.makeText(
+                    this@MainActivity,
+                    "Authentication succeeded!", Toast.LENGTH_SHORT
+                )
+                    .show()
+                processCryptoOperation(result.cryptoObject)
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                Toast.makeText(
+                    this@MainActivity, "Authentication failed",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    private fun processCryptoOperation(cryptoObject: BiometricPrompt.CryptoObject?) {
+        val data = when (biometricType) {
+            EncryptText -> getEncryptedText(cryptoObject)
+            DecryptText -> getDecryptedText(cryptoObject)
+            None -> EMPTY_STRING
+        }
+        binding.contentMain.layoutCrypto.tvEncrypted.text = data
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+    private fun getEncryptedText(cryptoObject: BiometricPrompt.CryptoObject?): String {
+        val text = binding.contentMain.layoutCrypto.etMessage.text.toString()
+        val encryptedData = cryptoObject?.cipher?.let {
+            return@let cryptographyManager.encryptData(text, it)
+        } ?: EncryptedData()
+        cipherText = encryptedData.cipherText ?: ByteArray(0)
+        initializationVector = encryptedData.initializationVector ?: ByteArray(0)
+        return String(cipherText, Charsets.UTF_8)
+    }
+
+    private fun getDecryptedText(cryptoObject: BiometricPrompt.CryptoObject?): String {
+        return cryptoObject?.cipher?.let {
+            return@let cryptographyManager.decryptData(cipherText, it)
+        } ?: EMPTY_STRING
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        setupViews()
+    }
+
+    private fun setupViews() {
+        setupMessageTv()
+        setupEnrollButton()
+        setupCryptoLayout()
+        setupFabButton()
+    }
+
+    private fun setupMessageTv() {
+        binding.contentMain.layoutBiometric.tvMessage.text = biometricHelper.getBiometricsAvailability()
+    }
+
+    private fun setupEnrollButton() {
+        binding.contentMain.layoutBiometric.btnEnrollBiometrics.visibility =
+            if (biometricHelper.canEnrollBiometrics) View.VISIBLE else View.GONE
+        binding.contentMain.layoutBiometric.btnEnrollBiometrics.setOnClickListener { launchEnrollmentBiometricsView() }
+    }
+
+    private fun launchEnrollmentBiometricsView() {
+        val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+            putExtra(
+                Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.DEVICE_CREDENTIAL
+            )
+        }
+        startActivityForResult(enrollIntent, REQUEST_CODE)
+        finish()
+    }
+
+    private fun setupCryptoLayout() {
+        binding.contentMain.layoutCrypto.cryptoLayoutView.visibility =
+            if (biometricHelper.canEnrollBiometrics) View.GONE else View.VISIBLE
+        binding.contentMain.layoutCrypto.btnEncrypt.setOnClickListener { encryptText() }
+        binding.contentMain.layoutCrypto.btnDecrypt.setOnClickListener { decryptData() }
+    }
+
+    private fun encryptText() {
+        biometricType = EncryptText
+        val cipher =
+            cryptographyManager.getInitializedCipherForEncryption(BuildConfig.secretKeyName)
+        biometricHelper.authenticate(cipher)
+    }
+
+    private fun decryptData() {
+        biometricType = DecryptText
+        val cipher =
+            cryptographyManager.getInitializedCipherForDecryption(
+                BuildConfig.secretKeyName,
+                initializationVector
+            )
+        biometricHelper.authenticate(cipher)
+    }
+
+    private fun setupFabButton() {
+        binding.fab.setOnClickListener {
+            biometricType = None
+            biometricHelper.authenticate()
         }
     }
 }
